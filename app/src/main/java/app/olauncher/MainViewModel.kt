48d98e6bc17b370b0ee734d8663ca6bb861fadb1
Shivam Ilasariya <76865039+shivamjj65@users.noreply.github.com>
2023-05-16 14:42:42 +0530

    Add files via upload

---
+++ app/src/main/java/app/olauncher/MainViewModel.kt
@@ -0,0 +1,213 @@
+package app.olauncher
+
+import android.app.Application
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.LauncherApps
+import android.os.UserHandle
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.work.*
+import app.olauncher.data.AppModel
+import app.olauncher.data.Constants
+import app.olauncher.data.Prefs
+import app.olauncher.helper.*
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val appContext by lazy { application.applicationContext }
+    private val prefs = Prefs(appContext)
+
+    val firstOpen = MutableLiveData<Boolean>()
+    val refreshHome = MutableLiveData<Boolean>()
+    val toggleDateTime = MutableLiveData<Unit>()
+    val updateSwipeApps = MutableLiveData<Any>()
+    val appList = MutableLiveData<List<AppModel>?>()
+    val hiddenApps = MutableLiveData<List<AppModel>?>()
+    val isOlauncherDefault = MutableLiveData<Boolean>()
+    val launcherResetFailed = MutableLiveData<Boolean>()
+    val homeAppAlignment = MutableLiveData<Int>()
+    val showMessageDialog = MutableLiveData<String>()
+
+    fun selectedApp(appModel: AppModel, flag: Int) {
+        when (flag) {
+            Constants.FLAG_LAUNCH_APP -> {
+                launchApp(appModel.appPackage, appModel.activityClassName, appModel.user)
+            }
+            Constants.FLAG_HIDDEN_APPS -> {
+                launchApp(appModel.appPackage, appModel.activityClassName, appModel.user)
+            }
+            Constants.FLAG_SET_HOME_APP_1 -> {
+                prefs.appName1 = appModel.appLabel
+                prefs.appPackage1 = appModel.appPackage
+                prefs.appUser1 = appModel.user.toString()
+                prefs.appActivityClassName1 = appModel.activityClassName
+                refreshHome(false)
+            }
+            Constants.FLAG_SET_HOME_APP_2 -> {
+                prefs.appName2 = appModel.appLabel
+                prefs.appPackage2 = appModel.appPackage
+                prefs.appUser2 = appModel.user.toString()
+                prefs.appActivityClassName2 = appModel.activityClassName
+                refreshHome(false)
+            }
+            Constants.FLAG_SET_HOME_APP_3 -> {
+                prefs.appName3 = appModel.appLabel
+                prefs.appPackage3 = appModel.appPackage
+                prefs.appUser3 = appModel.user.toString()
+                prefs.appActivityClassName3 = appModel.activityClassName
+                refreshHome(false)
+            }
+            Constants.FLAG_SET_HOME_APP_4 -> {
+                prefs.appName4 = appModel.appLabel
+                prefs.appPackage4 = appModel.appPackage
+                prefs.appUser4 = appModel.user.toString()
+                prefs.appActivityClassName4 = appModel.activityClassName
+                refreshHome(false)
+            }
+            Constants.FLAG_SET_HOME_APP_5 -> {
+                prefs.appName5 = appModel.appLabel
+                prefs.appPackage5 = appModel.appPackage
+                prefs.appUser5 = appModel.user.toString()
+                prefs.appActivityClassName5 = appModel.activityClassName
+                refreshHome(false)
+            }
+            Constants.FLAG_SET_HOME_APP_6 -> {
+                prefs.appName6 = appModel.appLabel
+                prefs.appPackage6 = appModel.appPackage
+                prefs.appUser6 = appModel.user.toString()
+                prefs.appActivityClassName6 = appModel.activityClassName
+                refreshHome(false)
+            }
+            Constants.FLAG_SET_HOME_APP_7 -> {
+                prefs.appName7 = appModel.appLabel
+                prefs.appPackage7 = appModel.appPackage
+                prefs.appUser7 = appModel.user.toString()
+                prefs.appActivityClassName7 = appModel.activityClassName
+                refreshHome(false)
+            }
+            Constants.FLAG_SET_HOME_APP_8 -> {
+                prefs.appName8 = appModel.appLabel
+                prefs.appPackage8 = appModel.appPackage
+                prefs.appUser8 = appModel.user.toString()
+                prefs.appActivityClassName8 = appModel.activityClassName
+                refreshHome(false)
+            }
+            Constants.FLAG_SET_SWIPE_LEFT_APP -> {
+                prefs.appNameSwipeLeft = appModel.appLabel
+                prefs.appPackageSwipeLeft = appModel.appPackage
+                prefs.appUserSwipeLeft = appModel.user.toString()
+                prefs.appActivityClassNameSwipeLeft = appModel.activityClassName
+                updateSwipeApps()
+            }
+            Constants.FLAG_SET_SWIPE_RIGHT_APP -> {
+                prefs.appNameSwipeRight = appModel.appLabel
+                prefs.appPackageSwipeRight = appModel.appPackage
+                prefs.appUserSwipeRight = appModel.user.toString()
+                prefs.appActivityClassNameRight = appModel.activityClassName
+                updateSwipeApps()
+            }
+        }
+    }
+
+    fun firstOpen(value: Boolean) {
+        firstOpen.postValue(value)
+    }
+
+    fun refreshHome(appCountUpdated: Boolean) {
+        refreshHome.value = appCountUpdated
+    }
+
+    fun toggleDateTime() {
+        toggleDateTime.postValue(Unit)
+    }
+
+    private fun updateSwipeApps() {
+        updateSwipeApps.postValue(Unit)
+    }
+
+    private fun launchApp(packageName: String, activityClassName: String?, userHandle: UserHandle) {
+        val launcher = appContext.getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
+        val activityInfo = launcher.getActivityList(packageName, userHandle)
+
+        val component = if (activityClassName.isNullOrBlank()) {
+            // activityClassName will be null for hidden apps.
+            when (activityInfo.size) {
+                0 -> {
+                    appContext.showToast(appContext.getString(R.string.app_not_found))
+                    return
+                }
+                1 -> ComponentName(packageName, activityInfo[0].name)
+                else -> ComponentName(packageName, activityInfo[activityInfo.size - 1].name)
+            }
+        } else {
+            ComponentName(packageName, activityClassName)
+        }
+
+        try {
+            launcher.startMainActivity(component, userHandle, null, null)
+        } catch (e: SecurityException) {
+            try {
+                launcher.startMainActivity(component, android.os.Process.myUserHandle(), null, null)
+            } catch (e: Exception) {
+                appContext.showToast(appContext.getString(R.string.unable_to_open_app))
+            }
+        } catch (e: Exception) {
+            appContext.showToast(appContext.getString(R.string.unable_to_open_app))
+        }
+    }
+
+    fun getAppList(includeHiddenApps: Boolean = false) {
+        viewModelScope.launch {
+            appList.value = getAppsList(appContext, prefs, includeHiddenApps)
+        }
+    }
+
+    fun getHiddenApps() {
+        viewModelScope.launch {
+            hiddenApps.value = getHiddenAppsList(appContext, prefs)
+        }
+    }
+
+    fun isOlauncherDefault() {
+        isOlauncherDefault.value = isOlauncherDefault(appContext)
+    }
+
+    fun resetDefaultLauncherApp(context: Context) {
+        resetDefaultLauncher(context)
+        launcherResetFailed.value = getDefaultLauncherPackage(appContext).contains(".")
+    }
+
+    fun setWallpaperWorker() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val uploadWorkRequest = PeriodicWorkRequestBuilder<WallpaperWorker>(8, TimeUnit.HOURS)
+            .setBackoffCriteria(BackoffPolicy.LINEAR, 1, TimeUnit.HOURS)
+            .setConstraints(constraints)
+            .build()
+        WorkManager
+            .getInstance(appContext)
+            .enqueueUniquePeriodicWork(
+                Constants.WALLPAPER_WORKER_NAME,
+                ExistingPeriodicWorkPolicy.REPLACE,
+                uploadWorkRequest
+            )
+    }
+
+    fun cancelWallpaperWorker() {
+        WorkManager.getInstance(appContext).cancelAllWork()
+        prefs.dailyWallpaperUrl = ""
+    }
+
+    fun updateHomeAlignment(gravity: Int) {
+        prefs.homeAlignment = gravity
+        homeAppAlignment.value = prefs.homeAlignment
+    }
+
+    fun showMessageDialog(message: String) {
+        showMessageDialog.postValue(message)
+    }
+}
---
+++ app/src/main/java/app/olauncher/data/Constants.kt
@@ -0,0 +1,83 @@
+package app.olauncher.data
+
+object Constants {
+
+    object Key {
+        const val FLAG = "flag"
+        const val RENAME = "rename"
+    }
+
+    object DateTime {
+        const val OFF = 0
+        const val ON = 1
+        const val DATE_ONLY = 2
+
+        fun isTimeVisible(dateTimeVisibility: Int): Boolean {
+            return dateTimeVisibility == ON
+        }
+
+        fun isDateVisible(dateTimeVisibility: Int): Boolean {
+            return dateTimeVisibility == ON || dateTimeVisibility == DATE_ONLY
+        }
+    }
+
+    object SwipeDownAction {
+        const val SEARCH = 1
+        const val NOTIFICATIONS = 2
+    }
+
+    object TextSize {
+        const val ONE = 0.5f
+        const val TWO = 0.75f
+        const val THREE = 0.9f
+        const val FOUR = 1f
+        const val FIVE = 1.1f
+        const val SIX = 1.25f
+        const val SEVEN = 1.5f
+    }
+
+    const val WALL_TYPE_LIGHT = "light"
+    const val WALL_TYPE_DARK = "dark"
+
+//    const val THEME_MODE_DARK = 0
+//    const val THEME_MODE_LIGHT = 1
+//    const val THEME_MODE_SYSTEM = 2
+
+    const val FLAG_LAUNCH_APP = 100
+    const val FLAG_HIDDEN_APPS = 101
+
+    const val FLAG_SET_HOME_APP_1 = 1
+    const val FLAG_SET_HOME_APP_2 = 2
+    const val FLAG_SET_HOME_APP_3 = 3
+    const val FLAG_SET_HOME_APP_4 = 4
+    const val FLAG_SET_HOME_APP_5 = 5
+    const val FLAG_SET_HOME_APP_6 = 6
+    const val FLAG_SET_HOME_APP_7 = 7
+    const val FLAG_SET_HOME_APP_8 = 8
+
+    const val FLAG_SET_SWIPE_LEFT_APP = 11
+    const val FLAG_SET_SWIPE_RIGHT_APP = 12
+
+    const val REQUEST_CODE_ENABLE_ADMIN = 666
+
+    const val HINT_RATE_US = 30
+    const val HINT_SHARE = 50
+
+    const val TRIPLE_TAP_DELAY_MS = 300L
+    const val LONG_PRESS_DELAY_MS = 500L
+
+    const val URL_ABOUT_OLAUNCHER = "https://tanujnotes.notion.site/Olauncher-Minimal-AF-4843e398b05a455bb521b0665b26fbcd"
+    const val URL_OLAUNCHER_PRIVACY = "https://tanujnotes.notion.site/Olauncher-Privacy-Policy-dd6ac5101ddd4b3da9d27057889d44ab"
+    const val URL_DOUBLE_TAP = "https://tanujnotes.notion.site/Double-tap-to-lock-Olauncher-0f7fb103ec1f47d7a90cdfdcd7fb86ef"
+    const val URL_OLAUNCHER_GITHUB = "https://www.github.com/shivamjj65"
+    const val URL_OLAUNCHER_PLAY_STORE = "https://play.google.com/store/apps/details?id=app.olauncher"
+    const val URL_PLAY_STORE_DEV = "https://play.google.com/store/apps/dev?id=7198807840081074933"
+    const val URL_TWITTER_TANUJ = "https://twitter.com/3_guna_lagaan_"
+    const val URL_INSTA_OLAUNCHER = "https://instagram.com/shivamm._.patel"
+    const val URL_WALLPAPERS = "https://gist.githubusercontent.com/tanujnotes/bf400a269746c5c124a599af040dd82e/raw"
+    const val URL_DEFAULT_DARK_WALLPAPER = "https://images.unsplash.com/photo-1512551980832-13df02babc9e"
+    const val URL_DEFAULT_LIGHT_WALLPAPER = "https://images.unsplash.com/photo-1515549832467-8783363e19b6"
+    const val URL_DUCK_SEARCH = "https://duck.co/?q="
+
+    const val WALLPAPER_WORKER_NAME = "WALLPAPER_WORKER_NAME"
+}
